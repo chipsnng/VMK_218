21f201f72b4f217f8b904d9c80567a2bb251295f
chipsnng <sqzs290306@gmail.com>
2026-09-14 01:04:32 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vmk218-v7';
+const CACHE_NAME = 'vmk218-v8';
 const ASSETS = ['./', './index.html', './manifest.json', './schedule.json'];
 
 self.addEventListener('install', (e) => {
